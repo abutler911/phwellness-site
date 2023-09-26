7e41c3b2f56fcb789f4c3fe6ba71c9aa6096397b
abutler911 <abutler911@gmail.com>
2023-09-25 21:42:14 -0600

    style changes

--- public/js/index.js
+++ public/js/index.js
@@ -18,18 +18,18 @@ const photos = [
 ];
 
 const testimonials = [
-  "I had an achy tennis elbow. I've had it for about 10 years. It's completely gone. &#128512",
-  "Knee pain gone! I can sit criss cross applesauce for the first time in like 6 years!",
-  "Being a very active person and playing pickleball 3-5 times a week, I developed some left hip issues. It was a deep pain/ache. After 2 weeks on the collagen, it hasn't come back!",
-  "For about a year, my right knee has had shooting pains when I would run, lift, squat or even sometimes go down stairs. Since starting this collagen, it hasn't hurt a bit!",
-  "5 days in I noticed the throbbing down my left leg from hip to knee gone. I could ALWAYS feel it while driving one car in particular AND after getting in bed at night. Gone...day 25 now, more pains gone little by little every day. I'm so so so happy!!",
-  "Torn miniscus pain...almost gone. It's been killing me for 3 months. Activate collagen just 2 weeks.",
-  "Softer skin, a glow, firm skin like my post pregnancy belly, higher metabolism, helped with weight loss.",
-  "My nails are very strong. Don't break or chip! My knees don't hurt going up and down stairs!! My skin is softer and cellulite in my legs has disappeared!! &#10084; &#10084; &#10084;",
-  "Love this collagen! Have tried powdered collagen but forgot about it or doesn't taste great. Have seen so many awesome improvements with this liquid collagen. Scar on my nose has diminished a lot!! Tried all kinds of scar creams with little to no change. My crepey skin on arms, neck and legs is gone!! Turkey neck gone! Definitely not going to stop using it! &#10084;",
-  "My neck skin is so much tighter!! My skin is more hydrated!! Less wrinkles!!!",
-  "My nails and hair are incredibly thicker! Also, my fine lines are minimized, and eye lids lifted slightly. MOST exciting, the dermatitis I had since January (horrible rash with itching burning blisters all over chin/jaw plus hundreds of bumps on forehead) is 90% GONE! I tried all sorts of things and nothing worked - I never expected the collagen to do this but I am so grateful! And my eyelashes are growing awesome too!",
-  "Outside healthier skin, the amazing joint pain reduction.",
+  "“I had an achy tennis elbow. I've had it for about 10 years. It's completely gone.” &#128512",
+  "“Knee pain gone! I can sit criss cross applesauce for the first time in like 6 years!”",
+  "“Being a very active person and playing pickleball 3-5 times a week, I developed some left hip issues. It was a deep pain/ache. After 2 weeks on the collagen, it hasn't come back!”",
+  "“For about a year, my right knee has had shooting pains when I would run, lift, squat or even sometimes go down stairs. Since starting this collagen, it hasn't hurt a bit!”",
+  "“5 days in I noticed the throbbing down my left leg from hip to knee gone. I could ALWAYS feel it while driving one car in particular AND after getting in bed at night. Gone...day 25 now, more pains gone little by little every day. I'm so so so happy!!”",
+  "“Torn miniscus pain...almost gone. It's been killing me for 3 months. Activate collagen just 2 weeks.”",
+  "“Softer skin, a glow, firm skin like my post pregnancy belly, higher metabolism, helped with weight loss.”",
+  "“My nails are very strong. Don't break or chip! My knees don't hurt going up and down stairs!! My skin is softer and cellulite in my legs has disappeared!! &#10084; &#10084; &#10084;”",
+  "“Love this collagen! Have tried powdered collagen but forgot about it or doesn't taste great. Have seen so many awesome improvements with this liquid collagen. Scar on my nose has diminished a lot!! Tried all kinds of scar creams with little to no change. My crepey skin on arms, neck and legs is gone!! Turkey neck gone! Definitely not going to stop using it! &#10084;”",
+  "“My neck skin is so much tighter!! My skin is more hydrated!! Less wrinkles!!!”",
+  "“My nails and hair are incredibly thicker! Also, my fine lines are minimized, and eye lids lifted slightly. MOST exciting, the dermatitis I had since January (horrible rash with itching burning blisters all over chin/jaw plus hundreds of bumps on forehead) is 90% GONE! I tried all sorts of things and nothing worked - I never expected the collagen to do this but I am so grateful! And my eyelashes are growing awesome too!”",
+  "“Outside healthier skin, the amazing joint pain reduction.”",
 ];
 
 const quoteContainer = document.querySelector("#quote");
